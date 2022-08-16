4892eaccef86d63952eb2692f15e76c65d48509b
EggcellentCode <dankim440@gmail.com>
2022-08-16 00:09:02 -0400

    Got up to final error handling in tier5 specs

--- server/db/User.js
+++ server/db/User.js
@@ -3,9 +3,71 @@ const db = require('./db');
 
 const User = db.define('user', {
   // Add your Sequelize fields here
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  userType: {
+    type: Sequelize.ENUM('STUDENT', 'TEACHER'),
+    defaultValue: 'STUDENT',
+    allowNull: false
+  },
+
+  isStudent: {
+    type: Sequelize.DataTypes.VIRTUAL,
+    get() {
+      return this.userType === 'STUDENT';
+    }
+  },
+
+  isTeacher: {
+    type: Sequelize.DataTypes.VIRTUAL,
+    get() {
+      return this.userType === 'TEACHER';
+    }
+  }
 
 });
 
+User.findUnassignedStudents = async function () {
+  return await User.findAll({
+    where: {
+      userType: 'STUDENT',
+      mentorId: null
+    }
+  });
+};
+
+User.findTeachersAndMentees = async function () {
+  return await User.findAll({
+    where: {
+      userType: 'TEACHER'
+    },
+    include: {
+      model: User,
+      as: 'mentees'
+    },
+  });
+};
+
+User.beforeUpdate(async function(updated) {
+  let mentors = await User.findByPk(updated.mentorId);
+  if (mentors !== null && mentors.userType === 'STUDENT') {
+    throw new Error (`cannot update ${mentors.name} as they are not a TEACHER`);
+  } else if ((await User.findByPk(updated.id)).mentorId !== null) {
+    throw new Error (`cannot change userType from STUDENT to TEACHER for ${updated.name}`)
+  } 
+  //what I tried!
+  //else if (await User.findTeachersAndMentees(updated.mentorId))...
+  return updated;
+  //could not figure out the final Hook conditional to check for mentees
+  //I tried to use our function above findTeachersAndMentees but I was getting an error
+});
 
 /**
  * We've created the association for you!
--- server/routes/users.js
+++ server/routes/users.js
@@ -18,4 +18,69 @@ const {
 
 // Add your routes here:
 
+router.get('/unassigned', async (req, res) => {
+  try {
+    res.json(await User.findUnassignedStudents());
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+router.get('/teachers', async (req, res) => {
+  try {
+    res.json(await User.findTeachersAndMentees());
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    if (isNaN(req.params.id)) {
+      res.sendStatus(400);
+    }
+    else {
+    let userInstance = await User.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+    if (userInstance) res.sendStatus(204);
+    if (!userInstance) res.sendStatus(404);
+  }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      res.sendStatus(404);
+    } else {
+      //I am assuming I am not able to send status 200 because my updated user function is not complete here
+      //But this is how I would handle 
+      res.status(200).send(user.updated(req.body));
+    }
+  } catch(error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const [users, wasCreated] = await User.findOrCreate({
+      where: req.body
+    });
+    if (wasCreated) {
+      res.status(201).send(users);
+    } else {
+      res.status(409).send('Incorrect status code');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
